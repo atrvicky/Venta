4d783950da4d360a55e2d034de02948dda6e5eaf
Muthu Vignesh <atrvicky@live.com>
2016-12-30 14:17:53 +0530

    Add JS to countdownTimer

--- Venta/scripts/main.js
+++ Venta/scripts/main.js
@@ -5,3 +5,33 @@ $(document).ready(function(){
     e.preventDefault();
   });
 });
+
+var timer = setInterval(clock, 1000);
+var days = 27;
+var hours = 20;
+var minutes = 46;
+var seconds = 27;
+
+function clock() {
+
+  console.log(seconds);
+
+  seconds--;
+  if (seconds == 0) {
+    minutes--;
+    seconds = 60;
+  }
+  if (minutes == 0) {
+    hours--;
+    minutes = 60;
+  }
+  if (hours == 0) {
+    days--;
+    hours = 24;
+  }
+
+document.getElementById('remDays').innerHTML = days;
+document.getElementById('remHours').innerHTML = hours;
+document.getElementById('remMinutes').innerHTML = minutes;
+document.getElementById('remSeconds').innerHTML = seconds;
+}
